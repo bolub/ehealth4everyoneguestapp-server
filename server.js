10c54f05ab080049120683f1e1c3a629e73afa8d
bolub <abiol5202@gmail.com>
2019-06-26 15:52:07 +0100

    transferring to another heroku instance

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express           = require('express');
 const app               = express();
-const PORT              = process.env.PORT || 3001;
+const PORT              = process.env.PORT;
 const cors              = require('cors');
 const path              = require('path');
 const guestRoutes       = require('./routes/guest');
